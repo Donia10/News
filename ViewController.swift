3a6b6b16a73a9bb6af4d3016b6f4cbf104ca22c2
Donia10 <donia.ashraf100@gmail.com>
2021-07-16 06:41:44 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Donia Ashraf on 7/16/21.
+//  Copyright © 2021 Donia Ashraf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
